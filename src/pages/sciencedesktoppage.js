311e9f9713d6aefc4ad45663122c05af62a5bbae
AnkushDX <ankushpanwar.dx@gmail.com>
2024-08-02 12:39:53 +0530

    1 more modal open

--- src/pages/sciencedesktoppage.js
+++ src/pages/sciencedesktoppage.js
@@ -11,6 +11,53 @@ const Sciencedesktoppage = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
   const [activeSlideIndex, setActiveSlideIndex] = useState(null);
 
+  // .............new modal....
+  const [show2, setShow2] = useState();
+  const [activeIndex, setActiveIndex] = useState(1);
+  const [selectedOption, setSelectedOption] = useState("1");
+  const [selectedOption2, setSelectedOption2] = useState("1");
+  const [selectedDay, setSelectedDay] = useState("30 Day");
+  const [isVisible, setIsVisible] = useState(false);
+  const [modalImage2, setModalImage2] = useState('/images/modalImgBrunoMd (1).webp');
+  const [modalTitle2, setModalTitle2] = useState('Royal Collagen Peptides');
+  const [modalsubTitle2, setModalsubTitle2] = useState('The Science of Beauty');
+
+
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+  // Function For Quantity Change Start //
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+  // Function For Quantity Change End //
+
+  // Function For Quantity Change Start //
+  const handleSelectChange2 = (event) => {
+    setSelectedOption2(event.target.value);
+  };
+  // Function For Quantity Change End //
+  // Function For Day Change Start //
+  const handleDayChange = (event) => {
+    setSelectedDay(event.target.value);
+  };
+  // Function For Day Change End //
+
+
+  const handleRadioChange = (index) => {
+    setActiveIndex(index);
+  };
+  const handleClose2 = () => setShow2(false);
+  const hanldeShow2 = (imageSrc2, title2, subTitle2) => {
+    setModalImage2(imageSrc2);
+    setModalTitle2(title2);
+    setModalsubTitle2(subTitle2)
+    setShow2(true)
+  }
+  // .............new modal....
+
+
 
   const settings = {
     dots: false, // Hide dots
@@ -47,6 +94,8 @@ const Sciencedesktoppage = () => {
     setShow(true);
     setActiveSlideIndex(index);
   };
+
+
   // modal function End.../
 
 
@@ -204,7 +253,7 @@ const Sciencedesktoppage = () => {
             </div>
 
             <div className={styles.slide2Img}>
-            <video width="" height="100%" loop autoPlay muted >
+              <video width="" height="100%" loop autoPlay muted >
                 <source src="/images/slidevideoA.mp4" type="video/mp4" />
                 <source src="/images/slidevideoA.mp4" type="video/ogg" />
               </video>
@@ -225,7 +274,7 @@ const Sciencedesktoppage = () => {
             </div>
 
             <div className={styles.slide2Img}>
-            <video width="" height="100%" loop autoPlay muted >
+              <video width="" height="100%" loop autoPlay muted >
                 <source src="/images/slidevideo3.mp4" type="video/mp4" />
                 <source src="/images/slidevideo3.mp4" type="video/ogg" />
               </video>
@@ -246,7 +295,7 @@ const Sciencedesktoppage = () => {
             </div>
 
             <div className={styles.slide2Img}>
-            <video width="" height="100%" loop autoPlay muted >
+              <video width="" height="100%" loop autoPlay muted >
                 <source src="/images/slidevideo2.mp4" type="video/mp4" />
                 <source src="/images/slidevideo2.mp4" type="video/ogg" />
               </video>
@@ -286,6 +335,167 @@ const Sciencedesktoppage = () => {
             <p>Natural Ingredients, clinically proven results</p>
 
           </div>
+{/* Modal start */}
+
+<Modal show={show2} onHide={handleClose2} backdrop="static" keyboard={false} size="lg" aria-labelledby="contained-modal-title-vcenter" centered >
+        <Modal.Header closeButton>
+
+        </Modal.Header>
+        <div className={styles.mainModalDiv}>
+
+          <div className={styles.modalImg}>
+            {/* <div className={styles.modalImgTitle}>
+          <p>Save an additional 10% use HEART at checkout</p>
+          </div> */}
+            <div className={styles.modalContentData}>
+              <div className={styles.brunoImgModal}>
+                <img src={modalImage2} alt='' />
+              </div>
+              <div className={styles.modaltitle}>
+                <h6>{modalTitle2}</h6>
+                <p>{modalsubTitle2}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.modalContent}>
+            <div className={`accordion ${styles.Accordian}`}>
+              <div className="accordion-item">
+                <div className={`${styles.Accordian1} ${activeIndex === 0 ? styles.active : ''}`}>
+                  <input
+                    type="radio"
+                    id="section1"
+                    name="accordion"
+                    checked={activeIndex === 0}
+                    onChange={() => handleRadioChange(0)}
+                  />
+                  <label className={`accordion-button ${styles.accordianButton}`} htmlFor="section1">
+                    ONE-TIME PURCHASE
+                    <div className={styles.price}>
+                      <span className={styles.dollar}>$</span>
+                      <span className={styles.mainPrice}>58</span>
+                      <span className={styles.cents}>.00</span>
+
+                    </div>
+                  </label>
+
+                </div>
+                {activeIndex === 0 && (
+                  <div className={`accordion-content ${styles.accordionContent}`}>
+                    <div className={styles.stocDiv}>
+                      <h6 className={styles.stock}>In Stock.</h6>
+                      <div className={styles.customSelect}>
+                        <select className={styles.selectDiv} value={selectedOption} onChange={handleSelectChange}>
+                          <option value="1" className={styles.qty}>
+                            Qty:1
+                          </option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                        </select>
+                        <p className={styles.unit}>1 Unit</p>
+                      </div>
+                      <div className={styles.addCartButton}><div>ADD TO CART</div> </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="accordion-item">
+                <div className={`${styles.Accordian2} ${activeIndex === 1 ? styles.active2 : ''}`}>
+                  <input
+                    className={styles.radioButton}
+                    type="radio"
+                    id="section2"
+                    name="accordion"
+                    checked={activeIndex === 1}
+                    onChange={() => handleRadioChange(1)}
+                  />
+                  <label className="accordion-button" htmlFor="section2">  SUBSCRIBE & SAVE 20%
+                    <div className={styles.price}>
+                      <span className={styles.dollar}>$</span>
+                      <span className={styles.mainPrice}>47</span>
+                      <span className={styles.cents}>.00</span>
+                      <span className={styles.offprice}>$58.98</span>
+                    </div>
+                  </label>
+                </div>
+                {activeIndex === 1 && (
+                  <div className={`accordion-content ${styles.accordionContent}`}>
+                    <div className={styles.saveContent}>
+                      <div className={styles.stocDiv2}>
+                        <div className={styles.save}>Save $11.80</div>
+                        <div className={styles.savetext}>with Subscription</div>
+                      </div>
+                      <div className={styles.sectionbar2}>
+                        <div>Don&apos;t miss this opportunity to save <b>20%</b> on this order and all future subscribe and save orders.</div>
+                        <ul>
+                          <li>No Fees</li>
+                          <li>Cancel anytime</li>
+                        </ul>
+                      </div>
+                      <div className={styles.sectionbar3}>
+                        <div className={styles.LearnbText} onClick={toggleVisibility} >Learn more
+                        </div>
+                        <div className={`${styles.UpArrow} ${isVisible ? styles.rotate90 : ''}`} onClick={toggleVisibility}>
+                          <svg class="flickity-button-icon" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow" transform="translate(100, 100) rotate(180) "></path></svg>
+                        </div>
+                      </div>
+                      {isVisible && (
+                        <div>
+                          <p className={styles.desclimerText}>
+                            There is no commitment and no minimum to buy. Please note
+                            exact shipment times may vary. To customize future
+                            shipments and changes, access your account in the Bruno MD
+                            portal, or email customer care department at{' '}
+                            <a href="mailto:customercare@brunopharma.com">
+                              customercare@brunopharma.com
+                            </a>
+                            .
+                          </p>
+                          <div className={styles.sectionbar4}>
+                            <div className={styles.informatonText}>
+                              <p>HOW SUBSCRIBE & SAVE WORKS</p>
+                            </div>
+                            <div>
+                              <img className={styles.infoBtn} src="//cdn.shopify.com/s/files/1/0021/0573/7291/t/172/assets/information-button.svg?v=6467211743660140745" height="20" width="20" alt="Information button" />
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                      <div className={styles.stocDiv3}>
+                        <h6 className={styles.stock2}>In Stock.</h6>
+                        <div className={styles.customSelect2}>
+                          <select className={styles.selectDiv2} value={selectedOption2} onChange={handleSelectChange2}>
+                            <option value="1">Qty: 1</option>
+                            <option value="2">Qty: 2</option>
+                            <option value="3">Qty: 3</option>
+                          </select>
+                          <p className={styles.unit}>1 Unit</p>
+                        </div>
+
+                        <div className={styles.customSelect3}>
+                          <select className={styles.selectDiv3} value={selectedDay} onChange={handleDayChange}>
+                            <option value="30 Day">30 Day</option>
+                            <option value="60 Day">60 Day</option>
+                            <option value="90 Day">90 Day</option>
+                          </select>
+                        </div>
+                        <div className={styles.addCartButton}><div>SUBSCRIBE</div> </div>
+                      </div>
+
+                    </div>
+
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </Modal>
+      {/* Modale Open */}
+
+
           <Slider ref={c => (slider2 = c)}{...settings}>
             <div className={styles.slider3data}>
               <div className={styles.sliderContent}>
@@ -324,7 +534,7 @@ const Sciencedesktoppage = () => {
                 </div>
                 <hr className={styles.blackHr2} />
                 <div className={styles.btnDivBuy}>
-                  <div className={styles.roundBtn}><p>Buy Now</p></div>
+                  <div className={styles.roundBtn}><p onClick={() => hanldeShow2('/images/modalImgBrunoMd (1).webp', 'Royal Collagen Peptides', 'The Science of Beauty')}>Buy Now</p></div>
                 </div>
               </div>
             </div>
@@ -368,7 +578,7 @@ const Sciencedesktoppage = () => {
                 </div>
                 <hr className={styles.blackHr2} />
                 <div className={styles.btnDivBuy}>
-                  <div className={styles.roundBtn}><p>Buy Now</p></div>
+                  <div className={styles.roundBtn} ><p onClick={() => hanldeShow2('/images/modalImgBrunoMd2.webp', 'CholestQ10 60 v-caps', 'The Science of Heart Health')}>Buy Now</p></div>
                 </div>
               </div>
             </div>
@@ -418,7 +628,7 @@ const Sciencedesktoppage = () => {
                 </div>
                 <hr className={styles.blackHr2} />
                 <div className={styles.btnDivBuy}>
-                  <div className={styles.roundBtn}><p>Buy Now</p></div>
+                  <div className={styles.roundBtn}><p onClick={() => hanldeShow2('/images/modalImgBrunoMd3.webp', 'Riboflam 90 v-caps', 'The Science of Longevity')}>Buy Now</p></div>
                 </div>
               </div>
             </div>
@@ -462,7 +672,7 @@ const Sciencedesktoppage = () => {
                 </div>
                 <hr className={styles.blackHr2} />
                 <div className={styles.btnDivBuy}>
-                  <div className={styles.roundBtn}><p>Buy Now</p></div>
+                  <div className={styles.roundBtn}><p  onClick={() => hanldeShow2('/images/modalImgBrunoMd4.webp', 'Bluerex Vision 60 softgels', 'The Science of Sight')}>Buy Now</p></div>
                 </div>
               </div>
             </div>
