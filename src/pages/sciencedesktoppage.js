7cff9aad9aee7aa8f6dee7dd25edc6bd3ed3e072
AnkushDX <ankushpanwar.dx@gmail.com>
2024-08-13 15:41:36 +0530

    bug fixed Mobile page

--- src/pages/sciencedesktoppage.js
+++ src/pages/sciencedesktoppage.js
@@ -94,7 +94,7 @@ const Sciencedesktoppage = () => {
         </div>
 
       </Modal>
-      <Carousel fade={true} interval={400000000} controls={false} pause={false} className={styles.carousel}>
+      <Carousel fade={true} interval={4000} controls={false} pause={false} className={styles.carousel}>
         {slides.map((slide, index) => (
           <Carousel.Item key={index}>
             <div className={styles.carouselMain}>
