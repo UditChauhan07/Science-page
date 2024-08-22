dbd158f6b1ba43ae97634bf8843fa591dace6830
AnkushDX <ankushpanwar.dx@gmail.com>
2024-08-22 18:25:45 +0530

    science page loop v6

--- src/utilities/SciencePage/SciencedesktopPage/sciencedesktoppage.js
+++ src/utilities/SciencePage/SciencedesktopPage/sciencedesktoppage.js
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Carousel } from 'react-bootstrap';
 import styles from './sciencedesktop.module.css'
 // import Modal from 'react-bootstrap/Modal';
@@ -8,11 +8,14 @@ import TabScience from '../ScienceTab/DesktopTab/TabScience';
 import ScienceSlider2 from '@/utilities/SciencePage/SliderPage/ScienceSlider2';
 import SciencesSlider from '@/utilities/SciencePage/SliderPage/SciencesSlider';
 import ScienceNewVideo from '@/utilities/SciencePage/ScienceVideo/ScienceNewVideo';
+import { FaAngleLeft } from "react-icons/fa6";
+import { FaAngleRight } from "react-icons/fa6";
 import Modal from '../ModalSciencePage/Modal';
 
 
 const Sciencedesktoppage = () => {
-  const [show, setShow] = useState();
+  const carouselRef = useRef(null);
+
   const [activeSlideIndex, setActiveSlideIndex] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -24,6 +27,20 @@ const Sciencedesktoppage = () => {
     setActiveSlideIndex(index);
     setIsModalOpen(true);
   };
+
+
+  const handlePrev = () => {
+    if (carouselRef.current) {
+      carouselRef.current.prev();
+    }
+  };
+
+  const handleNext = () => {
+    if (carouselRef.current) {
+      carouselRef.current.next();
+    }
+  };
+
 
   // ....Modal function Start..//
   // const handleClose = () => setShow(false);
@@ -109,30 +126,46 @@ const Sciencedesktoppage = () => {
         </Modal>
       )}
       {/* End of Custom Modal */}
+      <div className={styles.carouselContainer}>
 
-
-      <Carousel fade={true} interval={4000} controls={false} pause={false} className={styles.carousel}>
-        {slides.map((slide, index) => (
-          <Carousel.Item key={index}>
-            <div className={styles.carouselMain}>
-              <img src={slide.image} alt={`Slide ${index + 1}`} />
-            </div>
-            <Carousel.Caption className={styles.imgcotent}>
-              <div className={`container ${styles.heroImgContent}`}>
-                <h1>{slide.title}</h1>
-                <h2>{slide.subtitle}</h2>
-                <p>{slide.content}</p>
-
-                {slide.readmore && (
-                  <div className={styles.ancorLink}>
-                    <button onClick={() => handleShow(index)}>Read More</button>
-                  </div>
-                )}
+        <Carousel fade={true} interval={4000} controls={false} pause={false} className={styles.carousel} ref={carouselRef}>
+          {slides.map((slide, index) => (
+            <Carousel.Item key={index}>
+              <div className={styles.carouselMain}>
+                <img src={slide.image} alt={`Slide ${index + 1}`} />
               </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+              <Carousel.Caption className={styles.imgcotent}>
+                <div className={`container ${styles.heroImgContent}`}>
+                  <h1>{slide.title}</h1>
+                  <h2>{slide.subtitle}</h2>
+                  <p>{slide.content}</p>
+
+                  {slide.readmore && (
+                    <div className={styles.ancorLink}>
+                      <button onClick={() => handleShow(index)}>Read More</button>
+                    </div>
+                  )}
+                </div>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+
+        </Carousel>
+        <div className={styles.carouselBtnDiv}>
+        <button
+          className={`${styles.carouselControl} ${styles.carouselControlLeft}`}
+          onClick={handlePrev}
+        >
+         <i className={styles.LeftArrow}><FaAngleLeft /></i> 
+        </button>
+        <button
+          className={`${styles.carouselControl} ${styles.carouselControlRight}`}
+          onClick={handleNext}>
+
+         <i className={styles.RightArrow}><FaAngleRight /></i> 
+        </button>
+        </div>
+      </div>
       <div className={styles.RectangleDiv}>
         <div className={styles.Rectangle1}><img src='/images/Rectangle1.webp' /></div>
         <div className={styles.Rectangle2}><img src='/images/Rectangle2.webp' /></div>
